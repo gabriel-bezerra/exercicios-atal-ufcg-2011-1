1e13a8585a570aa41243121e0140e950b9b81533
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-04-15 23:25:28 -0300

    texto da questao

--- lista-exercicios-2/src/Questao02b.java
+++ lista-exercicios-2/src/Questao02b.java
@@ -1,4 +1,23 @@
 
+/*
+ * Desenvolva um algoritmo VerificaCiclo(No) que recebe um No representando a
+ * cabeça de uma lista simplesmente encadeada e retorna um booleano  indicando
+ * se essa lista possui um ciclo.
+ * 
+ * Obedeça as restrições abaixo e analise o algoritmo em relação ao custo de
+ * tempo e memória. Considere que cada No tem um valor inteiro (carga) e um
+ * apontador para o próximo No (prox).
+ * 
+ * Lembre-se que uma lista encadeada é
+ * cíclica se o último nó não aponta para NULL ou algum nó interno ou o último
+ * aopnta para um nó anterior.
+ * 
+ * Restrições:
+ * a. É possível alterar a estrutura de cada No.
+ * b. Não é possível alterar a estrutura de cada No.
+ * 
+ */
+
 public class Questao02b {
 
     /**
