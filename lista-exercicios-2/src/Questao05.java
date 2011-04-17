bffa4480a3d914e8150e81feb6a39a780c9d0797
Cleber <cleber@cleber-desktop.(none)>
2011-04-17 00:11:42 -0300

    solução da 5

--- lista-exercicios-2/src/Questao05.java
+++ lista-exercicios-2/src/Questao05.java
@@ -1,12 +1,35 @@
 
 public class Questao05 {
 
-    /**
-     * @param args
-     */
+	private static class Node {
+		private int value;
+		private Node left;
+		private Node right;
+
+		private Node(int value, Node left, Node right) {
+			this.value = value;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        Node root = new Node(23, null, null);
+        System.out.println("True: " + somaIgualA(root, 23));
+        root.left = new Node(1, null, null);
+        root.right = new Node(1, null, null);
+        System.out.println("False: " + somaIgualA(root, 23));
+        root.value = 22;
+        System.out.println("True: " + somaIgualA(root, 23));
 
     }
 
+    public static boolean somaIgualA(Node root, int x) {
+    	if (root == null || root.value > x) {return false;}
+        return somaIgualA(root.left, x - root.value) ||
+               somaIgualA(root.right, x - root.value) ||
+               (root.left == null && root.right == null && root.value == x);
+    }
+
+    
 }
