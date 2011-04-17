b5e4bad687689897e1e161940fb0d108dd7f46a4
Cleber <cleber@cleber-desktop.(none)>
2011-04-16 23:22:05 -0300

    comentário da questão 4

--- lista-exercicios-2/src/Questao04.java
+++ lista-exercicios-2/src/Questao04.java
@@ -1,4 +1,5 @@
 /*
+ * Questão 4:
  * Ugly Numbers são números cujos únicos fatores primos são 2, 3 ou 5. Ou seja,
  * é um número cuja decomposição em fatores primos só possui 2, 3 ou 5.
  * Desenvolva um algoritmo ehUgly(x) que, dado um inteiro como entrada, diga se
@@ -7,6 +8,18 @@
  *     ehUgly(12) = True.
  *     ehUgly(5) = True.
  *     ehUgly(14) = False.
+ * 
+ * Análise:
+ * No melhor caso, o número N não é divisível por 2, nem por 3, nem por 5,
+ * resultando num custo O(1). No pior caso, o algoritmo fará o máximo possível
+ * de divisões. Ora, o número de divisões (e o custo do algoritmo) é dado por
+ *     T(N) = log2(X) + log3(Y) + log5(Z),
+ * onde X, Y e Z são, respectivamente, as parcelas de N divisíveis por 2, 3 e 5.
+ * Mas, sabendo que X, Y e Z são menores que N, podemos fazer
+ *     T(N) <= log2(N) + log3(N) + log5(N)
+ * ou
+ *     T(N) = O(lg(N)).
+ * Com relação a espaço, temos um custo O(1);
  */
 
 public class Questao04 {
@@ -14,6 +27,7 @@ public class Questao04 {
         System.out.println("True: " + ehUgly(4));
         System.out.println("True: " + ehUgly(12));
         System.out.println("True: " + ehUgly(5));
+        System.out.println("False: " + ehUgly(14));
         System.out.println("False: " + ehUgly(14));
     }
 
