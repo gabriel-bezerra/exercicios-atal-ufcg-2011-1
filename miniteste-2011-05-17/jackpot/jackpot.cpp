949a48201d80d2d998e77a00acfd8de9772019fd
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-05-18 19:07:41 -0300

    solucao de jackpot

--- miniteste-2011-05-17/jackpot/jackpot.cpp
+++ miniteste-2011-05-17/jackpot/jackpot.cpp
@@ -15,10 +15,23 @@ int main() {
         bestBets[0] = bets[0];
 
         for (int i = 1; i < numberOfBets; ++i) {
-            bestBets[i] = bets[i] + ((bestBets[i - 1] > 0) ? bestBets[i - 1] : 0);
+            bestBets[i] = bets[i] + ((bestBets[i - 1] > 0) ? bestBets[i - 1]
+                                                           : 0);
         }
 
-        std::cout << bestBets[numberOfBets - 1] << std::endl;
+        int best = -10000;
+        for (int i = 0; i < numberOfBets; ++i) {
+            if (bestBets[i] > best) {
+                best = bestBets[i];
+            }
+        }
+
+        if (best > 0) {
+            std::cout << "The maximum winning streak is "
+                      << best << '.' << std::endl;
+        } else {
+            std::cout << "Losing streak." << std::endl;
+        }
 
         std::cin >> numberOfBets;
     }
