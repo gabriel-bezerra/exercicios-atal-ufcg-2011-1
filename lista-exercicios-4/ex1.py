b2dd1f12a7601100e5eb7076493dbf548c9e2f9c
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-06-16 17:36:49 -0300

    implementacao parcial do exercicio 1, apenas teste 2 passando

--- lista-exercicios-4/ex1.py
+++ lista-exercicios-4/ex1.py
@@ -1,7 +1,32 @@
 import unittest
 
 def balde(M, x, y):
-    pass
+    i = x - 1
+    j = y - 1
+    balde1(M, i, j, M[i][j])
+
+
+def balde1(M, x, y, antigo):
+    tinta = '-'
+
+    if not M[x][y] == antigo:
+        return
+
+    M[x][y] = tinta
+
+    # divisao
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+
+            # verifica se esta nos limites do array
+            if not (0 <= x + i < len(M) and
+                    0 <= y + j < len(M[x + i])):
+                return
+
+            # conquista
+            balde1(M, x + i, y + j, antigo)
+
+    # combinacao  trivial
 
 
 class BaldeTestCase(unittest.TestCase):
